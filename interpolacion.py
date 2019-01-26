393708215199783edd813713e6dd79271600bcfc
josera21 <jochix21@gmail.com>
2019-01-26 03:04:12 -0400

    Mejorado el algoritmo de Trapecio. Mas funciones añadidas.

--- interpolacion.py
+++ interpolacion.py
@@ -3,11 +3,9 @@ import matplotlib.pyplot as plt
 import decimal
 
 def interpolacion_newton(x, y, numEvaluar):
-	# Para precision en los calculos
-	decimal.getcontext().rounding = decimal.ROUND_DOWN
-
+	
 	if len(x) != len(y):
-		print "error!"
+		print("error!")
 	
 	n = len(x)
 	A = np.zeros( (n,n) )
@@ -15,17 +13,17 @@ def interpolacion_newton(x, y, numEvaluar):
 
 	for i in range(1,n):
 		for k in range(0, n-i):
-			a = decimal.Decimal(A[k+1,i-1])
-			b = decimal.Decimal(A[k,i-1])
+			a = A[k+1,i-1]
+			b = A[k,i-1]
 			A[k,i] = ( round(a, 8) - round(b, 8) ) / ( x[k+i] - x[k] )
-			print A
+			print(A)
 
-	print '** TABLA DE DIFERENCIAS FINITAS **'
-	print A
+	print('** TABLA DE DIFERENCIAS DIVIDIDAS **')
+	print(A)
 
 	coeficientes = []
 	for i in range(0,n):
-		bi = decimal.Decimal(A[0,i])
+		bi = A[0,i]
 		coeficientes.append(round(bi, 8))
 	print('COEFICIENTES DEL POLINOMIO: ', coeficientes)
 	
@@ -34,11 +32,10 @@ def interpolacion_newton(x, y, numEvaluar):
 	for i in range(1,n):
 		base = 1
 		for k in range(0,i):
-			base = decimal.Decimal(( numEvaluar - x[k] ) * base)
+			base = ( numEvaluar - x[k] ) * base
 			base = round(base, 8)
-			print('base', base)
 		
-		cal =  decimal.Decimal(coeficientes[i]*(base))
+		cal =  coeficientes[i]*(base)
 		valorInterpolado += round(cal,8)
 
 	return valorInterpolado
@@ -55,6 +52,6 @@ if __name__ == '__main__':
 	X = [1.0, 1.1, 1.2, 1.3, 1.4, 1.5]
 	Y = [0.8427, 0.8802, 0.9103, 0.9340, 0.9523, 0.9661 ]
 	resultado = interpolacion_newton(X, Y, 1.32)
-	print 'RESULTADO'
-	print resultado
+	print('RESULTADO')
+	print(resultado)
 	graficar(X, Y, 1.32, resultado)
--- trapecio.r
+++ trapecio.r
@@ -1,69 +1,125 @@
 fx <- function(x) {
-  return (1 / sqrt(2*pi)) * exp(-0.5*(x^2))
+  (1/sqrt(2*pi))*exp(-0.5*(x^2))
 }
 
-trapecio <- function(a, b, n) {
-  c <- numeric(n+1)
-  d <- numeric(1)
-  X <- numeric(n+1 )
+graficar <- function(prob, p, n) {
+  l=seq(-10,10,0.1)
+  x=seq(-4,4,0.1)
+  plot(x,fx(x),type="l",axes=FALSE,xlab="X",ylab="Y",col="1",main="Distribucion Normal Aproximada Con Trapecio")
+  axis(1)
+  axis(2)
+  abline(v=seq(-4,prob,0.001),col="red")
+  polygon(c(l,length(prob)),c(dnorm(l),length(prob)),col="white",border="black")
+  mtext(paste("P(Z<z)=",prob), adj=1)
+  mtext(paste("Para Z<=",p, " n=",n), side=4, adj=1)
+}
+
+trapecio <- function(a, b, n, f) {
+  c = numeric(n+1)
+  d = numeric(1)
+  X = numeric(n+1 )
 
   if (n == 1) {
-    h <- (b -a)
+    h = (b-a)
     
     for (i in 1:(n+1)) {
-      X[i] <- a + (i-1)*h
-      c[i] <- fx(X[i])
+      X[i] = a + (i-1)*h
+      c[i] = f(X[i])
     }
-    integral <- (h/2) * (c[1] + c[n+1])
+    integral = h/2*(c[1] + c[n+1])
   } else {
-    h <- (b-a)/n
+    h = (b-a)/n
     
     for(i in 1:(n+1)) {
-      X[i] <- a+(i-1)*h
-      c[i] <- fx(X[i])
+      X[i] = a+(i-1)*h
+      c[i] = f(X[i])
     }
     for(i in 2:n) {
-      d <- d + c[i]
+      d = d + c[i]
     }
-    integral <- (h/2) * (c[1] + 2*d + c[n+1])
+    integral = h/2*(c[1] + 2*d + c[n+1])
   }
 
   return (integral)
 }
 
-distribucionNormal <- function(p, n) {
-  tnormal = matrix(, 35, 1)
-  terror = matrix(, 35, 1)
-  tintegral = matrix(, 35, 1)
+tablaDistribucionNormal <- function(p, n) {
+  tnormal = matrix(,nrow=35, ncol=1, byrow=TRUE)
+  terror = matrix(,nrow=35, ncol=1, byrow=TRUE)
+  tintegral = matrix(,nrow=35, ncol=1, byrow=TRUE)
 
-  rownames(tnormal) <- seq(0, 3.4, 0.1)
-  colnames(tnormal) <- c("R")
-  colnames(terror) <- c("Error")
-  colnames(tintegral) <- c("Integral")
-  t <- seq(0, 3.5, 0.1)
+  rownames(tnormal) = seq(0, 3.4, by=0.1)
+  colnames(tnormal) = c("R")
+  colnames(terror) = c("Error")
+  colnames(tintegral) = c("Integral")
+  
+  t = seq(0, 3.5, 0.1)
 
   for (i in 1:35) {
-    rtrapecio <- trapecio(0, t[i], n)
-    tnormal[i,] <- rtrapecio + 0.5
-    terror[i,] <- (tnormal[i] - rtrapecio) / tnormal[i]
-    tintegral[i,] <- rtrapecio
+    rtrapecio = trapecio(0, t[i], n, f=fx)
+    tnormal[i,] = rtrapecio + 0.5
+    terror[i,] = (tnormal[i] - rtrapecio) / tnormal[i]
+    tintegral[i,] = rtrapecio
   }
 
   if(p < 0) {
-    prob <- trapecio(0, p, n)
-  } else if(p > 3.4) {
-    prob <- 1
+    prob = trapecio(0, p, n, f=fx)
+  } 
+  if(p > 3.4) {
+    prob = 1
   } else {
-    prob <- trapecio(0, p, n) + 0.5
+    prob = trapecio(0, p, n, f=fx) + 0.5
   }
 
-  tresultados <- cbind(tnormal, terror, tintegral)
-  imprimir <- list("Tabla Normal estandar con Trapecio" = tresultados, "Probabilidad P(X<x) " = prob)
+  graficar(prob, p, n)
+
+  tresultados = cbind(tnormal, terror, tintegral)
+  imprimir = list("Tabla Normal estandar con Trapecio" = tresultados, "Probabilidad P(Z<z) " = prob)
 
   return (imprimir)
 }
 
-p = 2.5
-n = 10 
-resultado <- distribucionNormal(p,n)
-resultado
+ejecutar <- function() {
+  opts <- c("Reproducir tabla distribucion normal", "Calcular Probabilidad", "Salir")
+  opts2 <- c("Calcular P{X<x}", "Calcular P{a<X<b}")
+  salir = FALSE
+
+  while(!salir) {
+    opc <- menu(opts, title="Ejemplos Distribucion normal con la Regla del Trapecio")
+
+    if(opc == 1) {
+      p = 1.1
+      n = 500 
+      resultado <- tablaDistribucionNormal(p,n)
+      print(resultado)
+    }
+    if(opc == 2) {
+      opc2 <- menu(opts2,  title="Elige una opcion para correr el ejemplo")
+
+      if(opc2 == 1) {
+        prob = trapecio(0, 5, 100, f=dnorm) + 0.5
+        print("--- RESULTADO ---")
+        print(paste("P (X<x): ", prob))
+        print("-----------------")
+      }
+      if(opc2 == 2) {
+        prob = trapecio(2, 7, 100, f=dnorm)
+        print("--- RESULTADO ---")
+        print(paste("P (a<X<b): ", prob))
+        print("-----------------")
+      }
+    }
+    if(opc == 3) {
+      salir = TRUE
+    }
+  }
+
+}
+
+ejecutar()
+
+#p = 1.1
+#n = 500 
+#resultado <- tablaDistribucionNormal(p,n)
+#resultado
+#print("Fin del calculo")
